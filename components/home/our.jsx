935397e95401840515613e6c3325e445b50dd6ab
Argha Chakrabarty <ac380012@gmail.com>
2021-05-30 14:45:43 +0530

    homepage done

--- components/home/our.jsx
+++ components/home/our.jsx
@@ -30,12 +30,12 @@ const Our = ({second, content, re, link}) => {
             <div className={`${styles["content"]} flex flex-col font-prompt text-font-color font-light text-2xl`}>
               {content}
               <Link href={link}>
-                <button className={`flex self-center items-center ${styles["button"]}`}>
+                <a className={`flex self-center items-center ${styles["button"]}`}>
                   <svg width="45" height="45" viewBox="0 0 54 54" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path d="M31.5371 5.37401L28.9208 7.99031L45.8355 24.905L1.84462 24.3185L1.89447 28.0573L45.8854 28.6438L29.4158 45.1134L32.1028 47.8004L53.0331 26.8701L31.5371 5.37401Z" fill="white" />
                   </svg>
                   <p>{re}</p>
-                </button>
+                </a>
               </Link>
             </div>
           </div>
